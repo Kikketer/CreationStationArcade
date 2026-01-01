d10ea10aae7b1abff580d60fdfb5fe3204359d8a
Chris Weed <chris@cjweed.com>
2026-01-01 00:44:17 -0600

    SyncBoat

--- launcher.sh
+++ launcher.sh
@@ -3,5 +3,5 @@ while true; do
     echo "Launching Menu"
     /home/pi/McAirpos/McAirpos/launCharc/launCharc nomap /home/pi/CreationStationArcade/menu.elf
     echo "Launching Game"
-    /home/pi/McAirpos/McAirpos/launCharc/launCharc nomap /home/pi/CreationStationArcade/games/Robob.elf
+    /home/pi/McAirpos/McAirpos/launCharc/launCharc nomap /home/pi/CreationStationArcade/games/SyncTheBoat.elf
 done
