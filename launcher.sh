bfd110ad66a5919586452ffc8521b7f6c5a3f0b5
Chris Weed <chris@cjweed.com>
2026-01-04 14:57:14 -0600

    four player test elf

--- launcher.sh
+++ launcher.sh
@@ -4,7 +4,7 @@ LOG_FILE="/home/pi/mcairpos.log"
 
 while true; do
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] Launching Menu" >> $LOG_FILE
-    /home/pi/McAirpos/McAirpos/launCharc/launCharc nomap verbose /home/pi/CreationStationArcade/originalMenu.elf >> $LOG_FILE 2>&1
+    /home/pi/McAirpos/McAirpos/launCharc/launCharc nomap verbose /home/pi/CreationStationArcade/games/four-player-test.elf >> $LOG_FILE 2>&1
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] Menu exited with status $?" >> $LOG_FILE
 
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] Launching Game" >> $LOG_FILE
